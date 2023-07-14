79a151c0f03669b8b074440195e9f1eb991a7d20
Van Quang <49120019+vanquanguetk63@users.noreply.github.com>
2023-07-14 21:12:31 +0700

    a

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 // var port = 8080;
 // var authRoute = require('../server/routes/auth.route');
-var userRoute = require('../server/routes/user.route.js');
-var addressRoute = require('../server/routes/address.route');
-var typeRoute = require('../server/routes/type.route');
-var postRoute = require('../server/routes/post.route');
-var buyRoute = require('../server/routes/buy.route');
-var houseRoute = require('../server/routes/house.route');
-var rentRoute = require('../server/routes/rent.route');
-var searchRoute = require('../server/routes/search.route');
+// var userRoute = require('./routes/user.route.js');
+// var addressRoute = require('../server/routes/address.route');
+// var typeRoute = require('../server/routes/type.route');
+// var postRoute = require('../server/routes/post.route');
+// var buyRoute = require('../server/routes/buy.route');
+// var houseRoute = require('../server/routes/house.route');
+// var rentRoute = require('../server/routes/rent.route');
+// var searchRoute = require('../server/routes/search.route');
 var adminRoute = require('../server/routes/admin.route');
 
-const upload = require('./multer');
-const cloundinary = require('./cloundinary');
-const fs = require('fs');
+// const upload = require('./multer');
+// const cloundinary = require('./cloundinary');
+// const fs = require('fs');
 
 // app.listen(port, function() {
 //     console.log("server is opened At " + port );
@@ -29,50 +29,54 @@ app.use(function(req, res, next) {
 
 // app.use('/api/auth', authRoute);
 
-app.use('/api/user', userRoute);
+// app.use('/api/user', userRoute);
 
-app.use('/api/address', addressRoute);
+// app.use('/api/address', addressRoute);
 
-app.use('/api/type', typeRoute);
+// app.use('/api/type', typeRoute);
 
-app.use('/api/post', postRoute);
+// app.use('/api/post', postRoute);
 
-app.use('/api/buy', buyRoute);
+// app.use('/api/buy', buyRoute);
 
-app.use('/api/house', houseRoute);
+// app.use('/api/house', houseRoute);
 
-app.use('/api/rent', rentRoute);
+// app.use('/api/rent', rentRoute);
 
-app.use('/api/search', searchRoute);
+// app.use('/api/search', searchRoute);
 
-app.use('/api/admin', adminRoute);
+app.use('/api/test', (req,res) => {
+  res.send({
+    success: true
+  } }
+});
 
 
-app.use('/upload-images', upload.array('image'), async (req, res) => {
-  const uploader = async (path) => await cloundinary.uploads(path, 'Images');
+// app.use('/upload-images', upload.array('image'), async (req, res) => {
+//   const uploader = async (path) => await cloundinary.uploads(path, 'Images');
 
-  if (req.method === 'POST')
-  {
-    const urls = []
+//   if (req.method === 'POST')
+//   {
+//     const urls = []
 
-    const files = req.files
+//     const files = req.files
 
-    for (const file of files) {
-      const { path } = file
-      const newPath = await uploader(path)
-      urls.push(newPath)
+//     for (const file of files) {
+//       const { path } = file
+//       const newPath = await uploader(path)
+//       urls.push(newPath)
 
-      fs.unlinkSync(path)
-    }
+//       fs.unlinkSync(path)
+//     }
     
-    res.status(200).json({
-      message:'Upload thành công',
-      data: urls
-    })
-  } else {
-    res.status(405).json({
-      err:'Upload không thành công'
-    })
-  }
-})
+//     res.status(200).json({
+//       message:'Upload thành công',
+//       data: urls
+//     })
+//   } else {
+//     res.status(405).json({
+//       err:'Upload không thành công'
+//     })
+//   }
+// })
 
